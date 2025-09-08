36ab5c32921bff0daff053dcdcb4ef37238939f8
Harsha <harshatg2004@gmail.com>
2025-09-08 18:10:49 +0530

    last commit

--- server/server.js
+++ server/server.js
@@ -53,12 +53,10 @@ app.use("/api/messages", messageRouter);
 // Connect to MongoDB
 await connectDB();
 
-if (process.env.NODE_ENV !== "production") {
-  const PORT = process.env.PORT || 5000;
-  server.listen(PORT, () =>
-    console.log(`Server is running on : http://localhost:${PORT}`)
-  );
-}
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () =>
+  console.log(`Server is running on : http://localhost:${PORT}`)
+);
 
 // Export server for Vercel
 export default server;
